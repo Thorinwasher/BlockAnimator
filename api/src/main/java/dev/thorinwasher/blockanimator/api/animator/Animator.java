19ff70095ba89a6dbe50ab542682b50a8a5f8abe
Thorinwasher <ragnarlol96@gmail.com>
2024-10-08 14:14:02 +0200

    Add emergency finish functionality

--- api/src/main/java/dev/thorinwasher/blockanimator/api/animator/Animator.java
+++ api/src/main/java/dev/thorinwasher/blockanimator/api/animator/Animator.java
@@ -8,6 +8,8 @@ import dev.thorinwasher.blockanimator.api.supplier.ImmutableVector3i;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3d;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -16,6 +18,8 @@ public class Animator<B> {
     private final Animation<B> animation;
     private final BlockAnimator<B> blockAnimator;
     private @Nullable Runnable onCompletion;
+    private boolean emergencyFinished;
+    private final List<ImmutableVector3i> placed = new ArrayList<>();
 
     public Animator(Animation<B> animation, BlockAnimator<B> blockAnimator) {
         this.animation = animation;
@@ -23,6 +27,9 @@ public class Animator<B> {
     }
 
     public boolean nextTick() {
+        if (emergencyFinished) {
+            return true;
+        }
         Animation.AnimationStatus status = animation.getStatus();
         if (status == Animation.AnimationStatus.COMPLETED) {
             Optional.ofNullable(onCompletion).ifPresent(Runnable::run);
@@ -39,7 +46,10 @@ public class Animator<B> {
             ImmutableVector3i identifier = entry.getKey();
             Vector3d position = entry.getValue().position();
             switch (entry.getValue().moveType()) {
-                case PLACE -> blockAnimator.blockPlace(identifier, supplier);
+                case PLACE -> {
+                    blockAnimator.blockPlace(identifier, supplier);
+                    placed.add(identifier);
+                }
                 case MOVE -> blockAnimator.blockMove(identifier, position, supplier, entry.getValue().transform());
                 case DESTROY -> blockAnimator.blockDestroy(identifier);
             }
@@ -49,6 +59,17 @@ public class Animator<B> {
 
     public void addOnCompletion(Runnable runnable) {
         this.onCompletion = runnable;
+    }
+
+    public void emergencyFinish() {
+        this.emergencyFinished = true;
+        List<ImmutableVector3i> toPlace = new ArrayList<>(animation.supplier().getPositions());
+        toPlace.removeAll(placed);
+        toPlace.forEach(animation.supplier()::placeBlock);
+        if (onCompletion != null) {
+            onCompletion.run();
+        }
+
     }
 }
 
